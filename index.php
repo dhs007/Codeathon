846b2406b0291722bb022bdf0ad5a46fa709f510
Darshil Shah <darshil98.shah@gmail.com>
2021-09-30 17:52:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,19 @@
 <body style="background-color:cyan; font-size:10px;">
 	<center><h1>WELCOME TO Blood Bank</h1></center>
 </body>
-
+<body style="background-color:cyan; font-size:10px;">
+	<center><h1>WELCOME TO Blood Bank</h1></center>
+</body>
+<body style="background-color:cyan; font-size:10px;">
+	<center><h1>WELCOME TO Blood Bank</h1></center>
+</body>
 <center>
 <a height="100" href="login.php">Login</a>
 </center>
 
+<center>
+<a href="logout.php">Logout</a>
+</center>
 <center>
 <a href="logout.php">Logout</a>
 </center>
